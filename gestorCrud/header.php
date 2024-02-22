0786d462035992fefd10b68ff67c5714f7e85dcd
Yodel23 <dawjosele@gmail.com>
2024-02-22 19:43:05 +0100

    Planificacion de carpetas v1

---
+++ gestorCrud/header.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+    <title>Sistema CRUD de inventario de productos</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <link rel="icon" type="image/x-icon" href="img/favicon.ico">
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
+</head>
+
+<body>
+    <header class="container-fluid bg-warning text-center py-3 display-4">
+        Sistema gestor de inventariado de productos
+    </header>
+    <main>
--- index.php
+++ index.php
@@ -1,46 +1,37 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-include "header.php";
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Autentificacion de usuarios</title>
+</head>
 
-?>
-
-<div class="container my-5 ">
-    <div class="row">
-        <div class="col text-center">
-            <div class="card">
-                <div class="card-header display-6">
-                    Acciones sobre BD
-                </div>
-                <div class="p-4">
-                    <div class="table-responsive">
-                        <table class="table table-primary">
-                            <thead>
-                                <tr>
-                                    <th scope="col">Alta de producto</th>
-                                    <th scope="col">Baja de producto</th>
-                                    <th scope="col">Actualizacion de producto</th>
-                                    <th scope="col">Listado de producto</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr class="">
-                                    <td scope="row"><a href="alta.php"><i class="bi-database-add px-3" style="font-size: 4rem; color: orange;"></i></a></td>
-                                    <td scope="row"><a href="baja.php"><i class="bi-database-dash px-3" style="font-size: 4rem; color: orange;"></i></a></td>
-                                    <td scope="row"><a href="actualiza.php"><i class="bi-database-check px-3" style="font-size: 4rem; color: orange;"></i></a></td>
-                                    <td scope="row"><a href="listado.php"><i class="bi-database-down px-3" style="font-size: 4rem; color: orange;"></i></a></td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-
-                </div>
+<body>
+    <form action='login.php' method='post'>
+        <fieldset>
+            <legend>Login</legend>
+            <div><span class='error'>
+                    <?php
+                    if (isset($error)) {
+                        echo $error;
+                    }
+                    ?>
+                </span></div>
+            <div class='fila'>
+                <label for='usuario'>Usuario:</label><br />
+                <input type='text' name='inputUsuario' id='usuario' maxlength="50" /><br />
             </div>
-        </div>
-    </div>
-</div>
+            <div class='fila'>
+                <label for='password'>Contraseña:</label><br />
+                <input type='password' name='inputPassword' id='password' maxlength="50" /><br />
+            </div>
+            <div class='fila'>
+                <input type='submit' name='enviar' value='Enviar' />
+            </div>
+        </fieldset>
+    </form>
 
-<?php
+</body>
 
-include "footer.php";
-
-?>
+</html>
