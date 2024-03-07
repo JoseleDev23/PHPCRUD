050d2a5b87fb223539a007dea073aeefc2a2689d
Yodel23 <dawjosele@gmail.com>
2024-03-07 21:16:17 +0100

    puesto rol en formulario y arreglo del redireccionamiento del login.php

--- gestorCrud/procesar_registro.php
+++ gestorCrud/procesar_registro.php
@@ -11,13 +11,14 @@ $password = $_POST["password"];
 $dni = $_POST["dni"];
 $edad = $_POST["edad"];
 $direccion = $_POST["direccion"];
+$rol = $_POST["rol"];
 
 // // Hash de la contraseña (puedes mejorar esto según tus necesidades)
 // $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
 // Inserta los datos en la tabla de usuarios
 $insertar = "INSERT INTO usuarios (nombre, correo_electronico, contrasena, dni, edad, direccion, rol) 
-             VALUES ('$nombre', '$email', '$password', '$dni', $edad, '$direccion', 'usuario')";
+             VALUES ('$nombre', '$email', '$password', '$dni', $edad, '$direccion', '$rol')";
 
 mysqli_query($conexion, $insertar);
 
