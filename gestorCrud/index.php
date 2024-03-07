ec915f5498f497d5b006c4faf1298cfa8afdf344
xocoduda <juan_xoco@hotmail.com>
2024-03-07 20:54:13 +0100

    trabajo en dashboard admin

--- gestorCrud/index.php
+++ gestorCrud/index.php
@@ -5,35 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Autentificacion de usuarios</title>
-    <style>
-        html {
-            background-color: #e2e2e2;
-        }
-
-        form {
-            max-width: 20%;
-            border-collapse: collapse;
-            border-radius: 10px;
-            box-shadow: 20px 20px 50px rgba(0, 0, 0, 0.1);
-            background-color: white;
-            margin: auto;
-            padding-top: 20px;
-            padding-bottom: 20px;
-            margin-top: 25px;
-            margin-bottom: 25px;
-            text-align: center;
-        }
-
-        form label {
-            display: block;
-            width: 80%;
-            margin: 0.2rem auto;
-        }
-
-        form button {
-            margin: .5em 2em;
-        }
-    </style>
 </head>
 
 <body>
--- gestorCrud/menu.php
+++ gestorCrud/menu.php
@@ -9,28 +9,45 @@ include "header.php";
         <div class="col text-center">
             <div class="card">
                 <div class="card-header display-6">
-                    Acciones sobre BD
+                    Gestión de libros
                 </div>
                 <div class="p-4">
                     <div class="table-responsive">
-                        <table class="table table-primary">
+                        <table class="table" style="text-align:center">
                             <thead>
                                 <tr>
-                                    <th scope="col">Alta de producto</th>
-                                    <th scope="col">Baja de producto</th>
-                                    <th scope="col">Actualizacion de producto</th>
-                                    <th scope="col">Listado de producto</th>
+                                    <th scope="col">Añadir libro</th>
+                                    <th scope="col">Eliminar libro</th>
+                                    <th scope="col">Actualizar libro</th>
+                                    <th scope="col">Listado de libros</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 <tr class="">
-                                    <td scope="row"><a href="alta.php"><i class="bi-database-add px-3" style="font-size: 4rem; color: orange;"></i></a></td>
-                                    <td scope="row"><a href="baja.php"><i class="bi-database-dash px-3" style="font-size: 4rem; color: orange;"></i></a></td>
-                                    <td scope="row"><a href="actualiza.php"><i class="bi-database-check px-3" style="font-size: 4rem; color: orange;"></i></a></td>
-                                    <td scope="row"><a href="listado.php"><i class="bi-database-down px-3" style="font-size: 4rem; color: orange;"></i></a></td>
+                                    <td scope="row">
+                                        <a href="alta.php">
+                                            <img src="../img/annadir.png" width="100px" height="100px">
+                                        </a>
+                                    </td>
+                                    <td scope="row">
+                                        <a href="baja.php">
+                                            <img src="../img/eliminar.png" width="100px" height="100px">
+                                        </a>
+                                    </td>
+                                    <td scope="row">
+                                        <a href="actualiza.php">
+                                            <img src="../img/actualizar.png" width="100px" height="100px">
+                                        </a>
+                                    </td>
+                                    <td scope="row">
+                                        <a href="listado.php">
+                                            <img src="../img/listado.png" width="100px" height="100px">
+                                        </a>
+                                    </td>
                                 </tr>
                             </tbody>
                         </table>
+                        <a href="menuPrincipalAdmin.php"> <i class="bi-arrow-return-left px-3" style="font-size: 4rem; color:black"></i></a>    
                     </div>
                 </div>
             </div>
