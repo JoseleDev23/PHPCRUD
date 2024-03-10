b15232c2bacaec1d038047c0b8c82c9ab59e3cd7
juan <juan_xoco@hotmail.com>
2024-03-10 13:29:41 +0100

    que se arregle por dios

--- gestorCrud/login.php
+++ gestorCrud/login.php
@@ -11,13 +11,18 @@ if (isset($_POST['inputUsuario'])) {
         $error = "Debes introducir un usuario y contraseña";
     } else {
         mysqli_select_db($conexion, "bibliotecaphp");
-        $query = "SELECT * FROM usuarios WHERE nombre = '$usuario' AND contrasena = '$password'";
+        $query = "SELECT nombre, contrasena, rol FROM usuarios WHERE nombre = '$usuario' AND contrasena = '$password'";
         $result = mysqli_query($conexion, $query);
 
         if ($result) {
             if (mysqli_num_rows($result) > 0) {
+                $usuarioData = mysqli_fetch_assoc($result);
                 $_SESSION['usuario'] = $usuario;
-                header("Location: dashboardAdmin/menuPrincipalAdmin.php");
+                if ($usuarioData['rol'] == 'admin') {
+                    header("Location: menuPrincipalAdmin.php");
+                } else {
+                    header("Location: menuPrincipalUser.php");
+                }
                 exit();
             } else {
                 $error = "Usuario o contraseña no válidos!";
