ec0929c1ef6bf0c2e0ad79c3f661745df9daa24d
Yodel23 <dawjosele@gmail.com>
2024-03-13 19:27:44 +0100

    Ultimos retoques

--- gestorCrud/dashboardAdmin/gestionLibros/actualiza.php
+++ gestorCrud/dashboardAdmin/gestionLibros/actualiza.php
@@ -8,7 +8,7 @@ include "header.php";
 <div class="container my-5 ">
     <div class="row">
         <div class="col text-center">
-        <div class="card shadow">
+            <div class="card shadow">
                 <div class="card-header display-6">
                     Actualizar libros
                 </div>
@@ -73,8 +73,21 @@ include "header.php";
 </div>
 </div>
 
-<?php
-
-include "footer.php";
-
-?>
+<div class="container-fluid p-0">
+    <footer class="text-center text-white" style="background-color: #663b2a">
+        <div class="container p-4 pb-0">
+            <p class="d-flex justify-content-center align-items-center">
+                <span class="me-3">¡Échale un ojo al GitHub!</span>
+                <a class="nav-link" href="https://github.com/Yodel23/PHPCRUD" target="_blank">
+                    <img src="../../../img/git.png" width="50px" height="50px" />
+                </a>
+            </p>
+        </div>
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+            <strong> Juan Antonio Buenestado Gómez & Jose Antonio Muñoz Sarmiento</strong>
+            © 2024 Todos los derechos reservados
+        </div>
+    </footer>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
